3ef22c754a1193b3392a8417d72c81d051012d81
Noah Eisen <ncteisen@gmail.com>
2014-11-21 14:28:06 -0500

    directory moving

---
+++ js/p2_main.js
@@ -0,0 +1,29 @@
+// run on page load
+$(document).ready(function () {
+
+	// create and run controller
+	var controller = new Controller(this)
+	var self = this
+
+	this.add_paragraph = function(text) {
+		$('#old_text').append("<p>" + text + <\p>)
+	}
+
+	// sends command to controller on enter
+	$("#cmd").keyup(function(event){
+    	if(event.keyCode == 13){
+
+  			// get user response
+        	var resp = $('#cmd').val();
+
+        	// reset input box
+        	$('#cmd').val('');
+
+        	// run engine and scroll down
+			controller.run(resp);
+			$(document).scrollTop($(document).height());
+    	}
+	});
+
+
+});
