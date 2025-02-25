bd56f04bb339abc2c5a91c472b96c551804e8347
Noah Eisen <noah.eisen@faire.com>
2025-02-25 10:46:29 -0800

    delete old script and update readme

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # Personal Website
 
-All files that are actually uploaded to AWS are in src/
-
-Anything else (LaTeX, Docx, etc) are stored in aux/
+This is my personal website.
--- aux/scripts/sync.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-# ensure we are at root
-cd "$(dirname "$0")/../.."
-
-aws s3 sync src/ s3://noaheisen.com  --exclude ".git/*" --exclude ".gitignore" --exclude "*.DS_Store"
